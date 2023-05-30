9934d8d4eca4c6745a2fd339126a7c19069542e0
Dipu Singh <dsingh197@gmail.com>
2023-05-30 18:58:15 +0530

    joi validation

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,25 +2,28 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 
 const secretKey = 'mysecretkey';
 
 const { loginUser } = require('../modal/loginModel');
 
-router.post('/', async (req, res) => {
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+});
 
-    // Find the user in the MongoDB collection
+router.post('/', async (req, res) => {
+    const { error, value } = loginSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map(item => item.message);  //Collection Errors
+        return res.status(400).json({ error: errorMessages });
+    }
 
     const loggedInUser = await loginUser(req.body);
-
-
-    // if (err) {
-    //     return res.status(500).json({ status: false, message: 'Error finding user' });
-    // }
     if (!loggedInUser) {
         return res.status(404).json({ status: false, message: 'User not found' });
     }
-
     // Compare the password
     const isMatch = await bcrypt.compare(req.body.password, loggedInUser.password);
 
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 
 const saltRounds = 10; // Number of salt rounds for hashing
 const secretKey = 'mysecretkey';
 const { registerUser } = require('../modal/loginModel');
 
+const registerSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().required(),
+    phone: Joi.string().required()
+});
+
+
 router.post('/', async (req, res) => {
+
+    const { error, value } = registerSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+        const errorMessages = error.details.map(item => item.message);  //Collection Errors
+        return res.status(400).json({ error: errorMessages });
+    }
+
+
     // Hash the password
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     // return console.log(hashedPassword)
